a0d76b1a1c526a000922acdacbaaf243692d9d8b
unknown <104089@thps.tp.edu.tw>
2021-05-23 11:40:28 +0800

    4h67buv5

---
+++ class/20210523/2.py
@@ -0,0 +1,15 @@
+'''
+a=int(input('上課時間:'))
+b=int(input('下課時間:'))
+print(a)
+print(b)
+print(b-a)
+
+a=int(input('距離:'))
+b=int(input('時間:'))
+print(a/b)
+'''
+a=int(input('數字:'))
+print((a+10)**2)
+print(a-(2/10))
+print((a*3)%10)
---
+++ class/20210523/題目.py
@@ -0,0 +1,38 @@
+#topic 1:
+a="10"
+b=4
+r=a*b
+print(r)
+print(type(r))
+#topic 2:
+a=10
+b=4
+r=a/b
+print(r)
+print(type(r))
+#topic 3:
+a=100
+b=4
+r=a/b
+print(r)
+print(type(r))
+#topic 4:
+a=6.6
+b=2
+r=(a/b)
+print(r)
+print(type(r))
+#topic 5:
+'''
+a="10"
+b="5"
+r=a/b
+print(r)
+print(type(r))
+'''
+#topic 6:
+a=True
+b=2
+r=a*b
+print(r)
+print(type(r))
